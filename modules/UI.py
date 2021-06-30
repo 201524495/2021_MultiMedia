346c42e892dca0f911e06ec7381869589fc0fbe2
201524495 <49744580+201524495@users.noreply.github.com>
2021-06-30 14:17:02 +0900

    update

---
+++ modules/UI.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '_uiFiles/example.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(368, 196)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(90, 50, 161, 41))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Show Videos"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+
--- modules/basics.py
+++ modules/basics.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import sys, multimedia2
+import sys, multimedia2, UI
 
 import PyQt5
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-from PyQt5 import uic
+# from PyQt5 import uic
 
-ExampleUI = '../_uiFiles/example.ui'
+# ExampleUI = '../_uiFiles/example.ui'
 
 
 def showVideos(button):
@@ -16,11 +16,11 @@ def showVideos(button):
     multimedia2.run()
 
 
-class MainDialog(QDialog):
+class MainDialog(QDialog, UI.Ui_Form):
     def __init__(self):
-        QDialog.__init__(self, None)
-        uic.loadUi(ExampleUI, self)
-
+        QDialog.__init__(self, None, Qt.WindowStaysOnTopHint)
+        # uic.loadUi(ExampleUI, self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(showVideos)
 
 
